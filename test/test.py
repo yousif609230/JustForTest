ef1730700420145d32d8c7112f78d8317fd44a97
yousif609230 <yousif.91.ahmed@gmail.com>
2024-11-19 11:47:47 +0300

    .

--- test/test.py
+++ test/test.py
@@ -3,9 +3,9 @@ import pandas as pd
 # Load the JSON file
 data = pd.read_json(r'C:\Users\admin\Documents\GitHub\JustForTest\test\alarm.json')
 
-# Print the number of rows and the first few rows
-print(f"Number of rows read: {len(data)}")
-print(data.head())  # Display the first few rows
+# # Print the number of rows and the first few rows
+# print(f"Number of rows read: {len(data)}")
+# print(data.head())  # Display the first few rows
 
 # Save to Excel file
 data.to_excel('test/alarm.xlsx', index=False)
